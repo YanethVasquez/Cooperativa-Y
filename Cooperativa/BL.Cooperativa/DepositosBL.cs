a793fb840c0b6ad0ddc4000f6537473bcf0a9b8e
YanethVasquez <cindypeNa@unah.hn>
2020-02-20 20:48:17 -0600

    segundo avance

---
+++ Cooperativa/BL.Cooperativa/DepositosBL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Cooperativa
+{
+    public class DepositosBL
+    {
+        public BindingList<Deposito> ListaDepositos { get; set; }
+
+        public DepositosBL()
+        {
+            ListaDepositos = new BindingList<Deposito>();
+
+            var deposito1 = new Deposito();
+            deposito1.Id = 1;
+            deposito1.Nombre = "Juancho";
+            deposito1.Cantidad = 5000;
+            deposito1.Ncuenta = 1503;
+            deposito1.Tcuenta = true;
+
+            ListaDepositos.Add(deposito1);
+
+            var deposito2 = new Deposito();
+            deposito2.Id = 2;
+            deposito2.Nombre = "Pedrito";
+            deposito2.Cantidad = 60000;
+            deposito2.Ncuenta = 0511;
+            deposito2.Tcuenta = true;
+
+            ListaDepositos.Add(deposito2);
+
+            var deposito3 = new Deposito();
+            deposito3.Id = 3;
+            deposito3.Nombre = "Juanita";
+            deposito3.Cantidad = 350;
+            deposito3.Ncuenta = 0102;
+            deposito3.Tcuenta = true;
+
+            ListaDepositos.Add(deposito3);
+        }
+        
+        public BindingList<Deposito> ObtenerDepositos()
+        {
+            return ListaDepositos; 
+        }
+    }
+
+    public class Deposito
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Cantidad { get; set; }
+        public int Ncuenta { get; set; }
+        public bool Tcuenta { get; set; }
+    }
+}
---
+++ Cooperativa/Cooperativa/FormDepositos.cs
@@ -0,0 +1,31 @@
+﻿using BL.Cooperativa;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cooperativa
+{
+    public partial class FormDepositos : Form
+    {
+        DepositosBL _depositos;
+
+        public FormDepositos()
+        {
+            InitializeComponent();
+
+            _depositos = new DepositosBL();
+            listaDepositosBindingSource.DataSource = _depositos.ObtenerDepositos();
+        }
+
+        private void FormDepositos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
